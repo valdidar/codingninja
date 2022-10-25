6ffa476644ae05aa9107c7084882fd18ab58c041
val didar singh <95515558+valdidar@users.noreply.github.com>
2022-10-25 17:47:04 +0530

    today work done

---
+++ fundamentals/src/fundamentals/HelloWorld.java
@@ -0,0 +1,10 @@
+package fundamentals;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("hello world");
+		System.out.println("hello world again");
+		System.out.println("hehe, i used syso command");
+		//this is a comment, we can also use ctrl+/ to make a line comment
+	}
+}
