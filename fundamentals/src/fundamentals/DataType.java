6129a4a9dbca9786eed6873f52bad5e5f68522fc
val didar singh <95515558+valdidar@users.noreply.github.com>
2022-10-27 01:00:03 +0530

    today hehe

---
+++ fundamentals/src/fundamentals/DataType.java
@@ -0,0 +1,18 @@
+package fundamentals;
+
+public class DataType {
+	short b=13;
+	byte c=14;
+	long d=13;
+	double e=14.5;
+	float f=12.234f;
+	char g='A';
+	boolean h=false;
+	public static void main(String[] args){
+		int a=10;
+		int b=12;
+		int c=a+b;
+		System.out.print(c);
+	}
+	
+}
--- fundamentals/src/fundamentals/HelloWorld.java
+++ fundamentals/src/fundamentals/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 		System.out.print("hello world");
 		System.out.println("hello world again");
 		System.out.println("hehe, i used syso command");
+		int a=10;
+		int b=15;
+		int c=a+b;
+		System.out.println(c);
 		//this is a comment, we can also use ctrl+/ to make a line comment
 	}
 }
