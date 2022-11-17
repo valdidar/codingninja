4ceeff285b2363350208e0a205fd96e4459ea49e
val didar singh <95515558+valdidar@users.noreply.github.com>
2022-11-17 20:15:19 +0530

    updated

---
+++ patterns/src/patterns/square.java
@@ -0,0 +1,18 @@
+package patterns;
+import java.util.Scanner;
+public class square {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int i=0;
+		while(i<n) {
+			int j=0;
+			while(j<n) {
+				System.out.print(n);
+				j++;
+			}
+			System.out.print("\n");
+			i++;
+		}
+	}
+}
