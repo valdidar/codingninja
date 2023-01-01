e17d2a0608c3bd56bdf0e5f7dc199ae5f0cb6521
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-01-01 23:48:28 +0530

    final and static

--- ddsa/src/classesandobjects/Student.java
+++ ddsa/src/classesandobjects/Student.java
@@ -2,16 +2,32 @@ package classesandobjects;
 
 public class Student{
 	public String name;
-	private int rollNumber;
-	public Student() {
-		
+	final private int rollNumber;
+	static int numStudent=0;
+	double cgpa;
+	final static double CONVERSIONFACTOR=0.95;
+	//static belong to class and available to all objects
+//	Student(String name, int rollNumber) {
+//		this.rollNumber=rollNumber;
+//		this.name=name;
+//		numStudent++;
+//	}
+	Student(String name) {
+		this.name=name;
+		numStudent++;
+		rollNumber=numStudent;
+	}
+	Student(){
+		name="default";
+		numStudent++;
+		rollNumber=numStudent;
 	}
 	public int getRollNumber() {
 		return rollNumber;
 	}
-	public void setRollNumber(int rollNumber) {
-		if(rollNumber<=0) 
-			return;
-		this.rollNumber=rollNumber;
-	}
+//	public void setRollNumber(int rollNumber) {
+//		if(rollNumber<=0) 
+//			return;
+//		this.rollNumber=rollNumber;
+//	}
 }
--- ddsa/src/classesandobjects/Studentuse.java
+++ ddsa/src/classesandobjects/Studentuse.java
@@ -1,13 +1,19 @@
 package classesandobjects;
 public class Studentuse{
 	public static void main(String[] args) {
-		Student s1=new Student();
+		Student s1=new Student("val didar singh");
 		System.out.println(s1);
-		s1.name="val didar singh";
-		s1.setRollNumber(2233);
-		
+//		s1.name="val didar singh";
+//		s1.setRollNumber(2233);
+		Student s2=new Student("girik");
+		Student s3=new Student();		
 		System.out.println(s1.name);
 		System.out.println(s1.getRollNumber());
+		System.out.println(s2.name);
+		System.out.println(s2.getRollNumber());
+		System.out.println(s3.name);
+		System.out.println(s3.getRollNumber());
+		System.out.println(Student.numStudent);
 	}
 	
 }
