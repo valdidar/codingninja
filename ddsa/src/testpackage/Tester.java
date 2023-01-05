45b0d1760fcb27c24d87ace5a866fb912c69f0b3
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-01-06 00:07:22 +0530

    more oops

---
+++ ddsa/src/testpackage/Tester.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+import classesandobjects.Test;
+
+public class Tester extends Test{
+
+	public static void main(String[] args) {
+		Tester c = new Tester();
+        c.name = "Ferrari";
+
+	}
+
+}
