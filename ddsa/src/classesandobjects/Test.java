3e8500ce3b16a9a36d50a949c986a87df08b7ec3
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-01-06 22:20:45 +0530

    oops completed

--- ddsa/src/classesandobjects/Test.java
+++ ddsa/src/classesandobjects/Test.java
@@ -3,3 +3,21 @@ package classesandobjects;
 public class Test {
 	protected String name;
 }
+
+//class A{
+//    public static void main(String[] args){
+//
+//    try{
+//       int a = 5/0;
+//    }
+//    catch(Exception e){
+//        System.out.print("Exception caught ");
+//    }
+//    catch(ArithmeticException e){
+//        System.out.print("Arithmetic Exception caught ");
+//   }
+//   finally{
+//       System.out.print("finally block");
+//   } 
+// }
+//}
