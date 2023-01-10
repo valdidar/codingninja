369c5eee1cab0046d3886626dcaba00d1a0fed1b
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-01-10 23:24:51 +0530

    OOPS 3 started

---
+++ ddsa/src/recursion/Interferance.java
@@ -0,0 +1,8 @@
+package recursion;
+
+public interface Interferance {
+	//we don't have to 	write public and abstract as they are already there
+	//we can't inherit interface from anything else, we have to make it from scratch
+	int pepepopo();
+	String name();
+}
